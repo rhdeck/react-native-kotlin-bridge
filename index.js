8c7243a200a4159b6f98ca07b6e93ac64ca50e90
Ray Deck <ray@raydeck.com>
2018-08-12 10:24:15 -0400

    Adding check to prevent replace crash

--- index.js
+++ index.js
@@ -286,6 +286,7 @@ function getBaseClass(obj, classes) {
   return baseClass ? baseClass : obj.baseClass;
 }
 function getReactPackageFromInterior(packageName, interior, imps) {
+  if (!packagename) return null;
   const obj = {
     packageName,
     interior,
